9f3ca970a252078e957958e2483173f3ca9ced2a
Madson369 <68554415+Madson369@users.noreply.github.com>
2021-09-23 14:53:57 -0300

    uma limpada e txt ensinando como inicia o server

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Status from "./components/Status.jsx";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { Toast } from "reactstrap";
 
 export default function App() {
 
